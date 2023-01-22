cb465b8e93495dffa4b3bf581ef6745a78b76020
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-01-22 07:12:23 -0500

    prod build

--- pages/api/socket.ts
+++ pages/api/socket.ts
@@ -38,7 +38,7 @@ const handler = async (req, res) => {
 
   // audio listener
   // ***RECORDINGS CAN ONLY LAST 5 MINUTES MAX (305 seconds)***
-  const recording = recorder.record();
+  const recording = recorder.record({ recorder: "arecord" });
 
   // speech-to-text
   let textFromClient;
