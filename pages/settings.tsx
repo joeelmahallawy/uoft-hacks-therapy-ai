b8210c6a256f9919f66da54ddfd4b7d6b86d6478
vaanyap <vaanyapuri1307@gmail.com>
2023-01-22 02:49:28 -0500

    Update settings.tsx

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -28,6 +28,10 @@ const Home = () => {
       </Button>
         <Demo />
        <TextInput label="Your email" placeholder="Your email" rightSection={<Loader size="xs" />} />;
+
+    <footer>
+        Call 
+    </footer>
     </>
   );
 };
