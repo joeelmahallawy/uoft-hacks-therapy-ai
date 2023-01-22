c37e78376b96c1c94991a00e520508508daa0e5b
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-01-22 04:45:45 -0500

    added chat page

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,77 +1,114 @@
-import { Box, Center, Flex, Text } from "@mantine/core";
-import { Button, Group } from "@mantine/core";
-import { Select, Image, Title } from "@mantine/core";
-import React from "react";
+import io from "Socket.IO-client";
 
-function Navbar() {
+import { Box, Button, Center, Flex, Text } from "@mantine/core";
+import { useEffect, useState } from "react";
+
+import { useSpeechSynthesis, useSpeechRecognition } from "react-speech-kit";
+import { Navbar } from "./home";
+
+let socket;
+const HomePage = () => {
+  // const { speak } = useSpeechSynthesis();
+  const [therapistResponse, setTherapistResponse] = useState("");
+  const [conversationContext, setConversationContext] = useState("");
+  const [isRecording, setIsRecording] = useState(false);
+
+  useEffect(() => {
+    // @ts-expect-error
+    if (!localStorage.getItem("settings")) window.location = "/onboarding";
+    socketInitializer();
+  }, [therapistResponse]);
+
+  useEffect(() => {
+    const speech = new SpeechSynthesisUtterance(therapistResponse);
+    speech.voice = window.speechSynthesis.getVoices()[10];
+
+    speech.pitch = 0.8;
+    speech.volume = 0.75;
+
+    speech.rate = 0.65;
+    window.speechSynthesis.speak(speech);
+  }, [therapistResponse]);
+
+  const socketInitializer = async () => {
+    await fetch("/api/socket");
+    socket = io();
+
+    socket.on("connect", () => {
+      console.log("Connected to socket!");
+    });
+
+    socket?.on("response", ({ therapistResponse, context }) => {
+      console.log(context);
+      setConversationContext(context);
+      setTherapistResponse(therapistResponse);
+    });
+  };
+
   return (
-    <>
-      <Center sx={{ padding: 20, justifyContent: "space-between" }}>
-        <Box sx={{}}>
-          <Title>(Placeholder image)</Title>
-        </Box>
-        <Center sx={{ gap: 30 }}>
-          <Button
-            size="lg"
-            sx={{ background: "none", "&:hover": { background: "none" } }}
-          >
-            Chat
-          </Button>
-          <Button
-            size="lg"
-            sx={{ background: "none", "&:hover": { background: "none" } }}
-          >
-            Account Settings
-          </Button>
-        </Center>
-      </Center>
-    </>
-  );
-}
-const IndexPage = () => {
-  return (
-    <>
+    <Box>
       <Navbar />
 
-      <Group position="center">
-        <Center mt={20}>
-          <Title
-            variant="gradient"
-            gradient={{ from: "#ed6ea0", to: "#00FFFF", deg: 35 }}
-          >
-            Hello you beautiful soul, welcome to my office!
-          </Title>
-        </Center>
-
-        <br></br>
-      </Group>
-      <Group
-        position="center"
-        style={{
-          display: "flex",
+      <Flex
+        sx={{
+          width: "75vw",
+          height: "70vh",
+          background: "white",
+          margin: "40px auto",
+          borderRadius: 10,
+          justifyContent: "space-between",
           flexDirection: "column",
-          alignItems: "center",
+          padding: 10,
         }}
       >
-        <Group mt={20}>
-          <Image
-            src="https://img.freepik.com/premium-photo/teddy-bears-with-bouquet-red-roses-black-background-selective-focus_309761-311.jpg?w=1480"
-            alt="Alternate text"
-            width="500px"
-          />
-        </Group>
-        <Group style={{ flex: 1 }}>
+        <ChatComponent
+          who={"Harmony"}
+          content={`Hello ${
+            typeof window !== "undefined" &&
+            JSON.parse(localStorage.getItem("settings")).name
+          }, what would you like to talk about today?`}
+        />
+
+        {isRecording ? (
           <Button
-            variant="gradient"
-            gradient={{ from: "#4B0082", to: "#00FFFF", deg: 35 }}
-            onClick={() => (window.location.href = "/chat")}
+            sx={{ width: "80%" }}
+            onClick={() => {
+              setIsRecording(false);
+              socket?.emit("pause-recording", { conversationContext });
+            }}
           >
-            Talk to me
+            STOP
           </Button>
-        </Group>
-      </Group>
-    </>
+        ) : (
+          <Button
+            sx={{ width: "80%", margin: "0 auto 10px auto" }}
+            onClick={async () => {
+              setIsRecording(true);
+              socket?.emit("listen", {
+                conversationContext,
+                patientName: typeof window !== "undefined" && localStorage,
+              });
+            }}
+          >
+            Start
+          </Button>
+        )}
+      </Flex>
+    </Box>
   );
 };
+export default HomePage;
 
-export default IndexPage;
+const ChatComponent = ({
+  who,
+  content,
+}: {
+  who: "Harmony" | string;
+  content: string;
+}) => {
+  return (
+    <Text color="black">
+      <span style={{ fontWeight: 600 }}>{who}:</span> {content}
+    </Text>
+  );
+};
