fb56103d6fd1b394ba8c6163865f058d2e4f63c1
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-01-21 21:48:19 -0500

    added speech

---
+++ lib/openai.ts
@@ -0,0 +1,30 @@
+// const { Configuration, OpenAIApi } = require("openai");
+import { Configuration, OpenAIApi } from "openai";
+
+export const conversate = async (
+  conversation
+): Promise<{ fullPrompt: string; therapistResponse: string }> => {
+  const configuration = new Configuration({
+    apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: conversation,
+    temperature: 0.7,
+    max_tokens: 3500,
+    top_p: 0.5,
+    frequency_penalty: 1,
+    presence_penalty: 1,
+  });
+
+  console.log(
+    JSON.parse(response.config.data).prompt +
+      `\n\n${response.data.choices[0].text}`
+  );
+  return {
+    fullPrompt: JSON.parse(response.config.data).prompt,
+    therapistResponse: response.data.choices[0].text,
+  };
+};
