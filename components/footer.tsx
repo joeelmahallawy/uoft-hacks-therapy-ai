1a0ad95bd5d861adc2786005cd2c3063892aaac9
joeelmahallawy <youssef.elmahallawy01@gmail.com>
2023-01-22 07:20:42 -0500

    dump

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,54 +1,69 @@
-import { createStyles, Text, Container, ActionIcon, Group, Image, Title, List } from '@mantine/core';
-import { IconBrandTwitter, IconBrandYoutube, IconBrandInstagram } from '@tabler/icons';
-import { MantineLogo } from '@mantine/ds';
-import logo from "../utils/HARMONY_AI.png"
+import {
+  createStyles,
+  Text,
+  Container,
+  ActionIcon,
+  Group,
+  Image,
+  Title,
+  List,
+} from "@mantine/core";
+import {
+  IconBrandTwitter,
+  IconBrandYoutube,
+  IconBrandInstagram,
+} from "@tabler/icons";
+import logo from "../utils/HARMONY_AI.png";
 
 const useStyles = createStyles((theme) => ({
   footer: {
     marginTop: 20,
     paddingTop: theme.spacing.xl * 2,
     paddingBottom: theme.spacing.xl * 2,
-    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
     borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+      theme.colorScheme === "dark" ? theme.colors.dark[5] : theme.colors.gray[2]
     }`,
   },
 
   logo: {
     maxWidth: 200,
 
-    [theme.fn.smallerThan('sm')]: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
+    [theme.fn.smallerThan("sm")]: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
     },
   },
 
   description: {
     marginTop: 5,
 
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       marginTop: theme.spacing.xs,
-      textAlign: 'center',
+      textAlign: "center",
     },
   },
 
   inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    display: "flex",
+    justifyContent: "space-between",
 
-    [theme.fn.smallerThan('sm')]: {
-      flexDirection: 'column',
-      alignItems: 'center',
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
+      alignItems: "center",
     },
   },
 
   groups: {
-    display: 'flex',
-    flexWrap: 'wrap',
+    display: "flex",
+    flexWrap: "wrap",
 
-    [theme.fn.smallerThan('sm')]: {
-      display: 'none',
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
     },
   },
 
@@ -57,14 +72,17 @@ const useStyles = createStyles((theme) => ({
   },
 
   link: {
-    display: 'block',
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[6],
+    display: "block",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
     fontSize: theme.fontSizes.sm,
     paddingTop: 3,
     paddingBottom: 3,
 
-    '&:hover': {
-      textDecoration: 'underline',
+    "&:hover": {
+      textDecoration: "underline",
     },
   },
 
@@ -73,27 +91,27 @@ const useStyles = createStyles((theme) => ({
     fontWeight: 700,
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
     marginBottom: theme.spacing.xs / 2,
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
   },
 
   afterFooter: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginTop: theme.spacing.xl,
     paddingTop: theme.spacing.xl,
     paddingBottom: theme.spacing.xl,
     borderTop: `1px solid ${
-      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+      theme.colorScheme === "dark" ? theme.colors.dark[4] : theme.colors.gray[2]
     }`,
 
-    [theme.fn.smallerThan('sm')]: {
-      flexDirection: 'column',
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
     },
   },
 
   social: {
-    [theme.fn.smallerThan('sm')]: {
+    [theme.fn.smallerThan("sm")]: {
       marginTop: theme.spacing.xs,
     },
   },
@@ -111,7 +129,7 @@ export default function Footer({ data }: FooterLinksProps) {
 
   const groups = data.map((group) => {
     const links = group.links.map((link, index) => (
-      <Text<'a'>
+      <Text<"a">
         key={index}
         className={classes.link}
         component="a"
@@ -132,29 +150,35 @@ export default function Footer({ data }: FooterLinksProps) {
 
   return (
     <footer className={classes.footer}>
-      
       <Container className={classes.afterFooter}>
         <Title order={1}>
-            <a href="/">
-
-        <Image m={10} mb={30} src={logo.src} width={200}/>
-            </a>
+          <a href="/">
+            <Image m={10} mb={30} src={logo.src} width={200} />
+          </a>
         </Title>
         <Text color="dimmed" size="sm">
-            
-        <List>
-            <Title color={'white'} order={2}>Hotline centers:</Title>
-      <List.Item>Clone or download repository from GitHub</List.Item>
-      <List.Item>Install dependencies with yarn</List.Item>
-      <List.Item>To start development server run npm start command</List.Item>
-      <List.Item>Run tests to make sure your changes do not break the build</List.Item>
-      <List.Item>Submit a pull request once you are done</List.Item>
-    </List>
+          <List>
+            <Title color={"white"} order={2}>
+              Hotline centers:
+            </Title>
+            <List.Item>Clone or download repository from GitHub</List.Item>
+            <List.Item>Install dependencies with yarn</List.Item>
+            <List.Item>
+              To start development server run npm start command
+            </List.Item>
+            <List.Item>
+              Run tests to make sure your changes do not break the build
+            </List.Item>
+            <List.Item>Submit a pull request once you are done</List.Item>
+          </List>
         </Text>
 
-        <Group spacing={0} className={classes.social} position="right" noWrap>
-          
-        </Group>
+        <Group
+          spacing={0}
+          className={classes.social}
+          position="right"
+          noWrap
+        ></Group>
       </Container>
     </footer>
   );
